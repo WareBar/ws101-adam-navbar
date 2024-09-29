93aea466fdfb8e7a05628394ff1ce6a48fb2d6a4
ChrisReformado <reformadchris20@gmail.com>
2024-09-29 15:29:51 -0800

    changed the banner

--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -3,7 +3,11 @@ import Content from "../components/Content.jsx"
 export default function NoPage(){
 	return (
 			<div className="NoPage">
-				<Content text="PAGE NOT FOUND: ERROR 404"/>
+				<Content
+				text="PAGE NOT FOUND: ERROR 404"
+				banner="https://i.redd.it/ih6e57ye7o3c1.gif"
+				
+				/>
 			</div>
 		)
 }
